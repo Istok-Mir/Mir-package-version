92aea7d625e5494807f09cf01cca2375f1e8d2af
Предраг Николић <idmpepe@gmail.com>
2025-06-11 19:42:46 +0200

    changes

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import sublime
 
 GITHUB_ASSET_VERSION = 'v0.0.7'
 server_storage = PackageStorage(tag='0.0.1')
+server_path = server_storage / "package-version-server" / 'package-version-server'
+
+async def package_storage_setup():
+    if server_path.exists():
+        return
+    await install()
 
 
 class PackageVersionServer(LanguageServer):
@@ -15,41 +21,42 @@ class PackageVersionServer(LanguageServer):
     }
 
     async def activate(self):
-        server_path = server_storage / "package-version-server" / 'package-version-server'
-        if not server_path.exists():
-            await self.install()
+        # setup runtime and install dependencies
+        await package_storage_setup()
 
         await self.connect('stdio', {
             'cmd': [server_path]
         })
 
-    async def install(self):
-        with LoaderInStatusBar(f'Installing {PackageVersionServer.name}'):
-            fetch_url, archive_filename = self._archive_on_github()
-            save_to = server_storage / archive_filename
-            await server_storage.download(fetch_url, save_to)
-            unzip(save_to, new_name='package-version-server')
 
-    def _archive_on_github(self) -> tuple[str, str]:
-        platform = sublime.platform()
-        arch = sublime.arch()
-        if platform == 'windows':
-            platform_code = 'pc-windows-msvc.zip'
-        elif platform == 'linux':
-            platform_code = 'unknown-linux-gnu.tar.gz'
-        elif platform == 'osx':
-            platform_code = 'apple-darwin.tar.gz'
-        else:
-            raise Exception('{} {} is not supported'.format(arch, platform))
-        if arch == 'x32':
-            arch_code = 'x86_64'
-        elif arch == 'x64':
-            arch_code = 'x86_64'
-        elif arch == 'arm64':
-            arch_code = 'aarch64'
-        else:
-            raise Exception('Unsupported architecture: {}'.format(arch))
-        archive_filename = 'package-version-server-{}-{}'.format(arch_code, platform_code) # Using 'deno-' prefix
+async def install():
+    with LoaderInStatusBar(f'Installing {PackageVersionServer.name}'):
+        fetch_url, archive_filename = archive_on_github()
+        save_to = server_storage / archive_filename
+        await server_storage.download(fetch_url, save_to)
+        unzip(save_to, new_name='package-version-server')
 
-        fetch_url = f'https://github.com/zed-industries/package-version-server/releases/download/{GITHUB_ASSET_VERSION}/{archive_filename}'
-        return fetch_url, archive_filename
+
+def _archive_on_github() -> tuple[str, str]:
+    platform = sublime.platform()
+    arch = sublime.arch()
+    if platform == 'windows':
+        platform_code = 'pc-windows-msvc.zip'
+    elif platform == 'linux':
+        platform_code = 'unknown-linux-gnu.tar.gz'
+    elif platform == 'osx':
+        platform_code = 'apple-darwin.tar.gz'
+    else:
+        raise Exception('{} {} is not supported'.format(arch, platform))
+    if arch == 'x32':
+        arch_code = 'x86_64'
+    elif arch == 'x64':
+        arch_code = 'x86_64'
+    elif arch == 'arm64':
+        arch_code = 'aarch64'
+    else:
+        raise Exception('Unsupported architecture: {}'.format(arch))
+    archive_filename = 'package-version-server-{}-{}'.format(arch_code, platform_code) # Using 'deno-' prefix
+
+    fetch_url = f'https://github.com/zed-industries/package-version-server/releases/download/{GITHUB_ASSET_VERSION}/{archive_filename}'
+    return fetch_url, archive_filename
